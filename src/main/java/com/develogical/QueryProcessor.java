88c9f15cc2e4c0b410b6b9fae81bbba7b5c6864f
Raj Sudra <rsudra@hotmail.com>
2020-06-24 15:42:26 +0100

    basic number support

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,10 @@
 package com.develogical;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class QueryProcessor {
 
     public String process(String query) {
@@ -8,7 +13,10 @@ public class QueryProcessor {
         } else if (query.toLowerCase().contains("what is your name")) {
             return "Unsafe";
         } else if (query.toLowerCase().contains("which of the following numbers is the largest")) {
-            return "20874";
+            String[] queryParts = query.split(":");
+            String[] numbers = queryParts[1].split(",");
+            int[] array = Arrays.asList(numbers).stream().mapToInt(Integer::parseInt).toArray();
+            return Integer.toString(maxValue(array));
         } else if (query.toLowerCase().contains("what is 6 plus 15")) {
             return "21";
         } else if (query.toLowerCase().contains("which of the following numbers is the largest: 450, 92")) {
@@ -17,5 +25,13 @@ public class QueryProcessor {
 
 
         return "";
+    }
+
+    public int maxValue(int array[]) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        return Collections.max(list);
     }
 }
